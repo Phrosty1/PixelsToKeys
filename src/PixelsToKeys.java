024e69c0fb47b6a086f26f5c7a0bbd1f3fc3d89a
Phrosty1 <istas.phrost@gmail.com>
2020-12-14 19:45:40 -0500

    wonky, but picks locks.... then kills people accidentally

--- src/PixelsToKeys.java
+++ src/PixelsToKeys.java
@@ -68,17 +68,19 @@ public class PixelsToKeys extends JFrame {
 
 	static private class KeyMapEntry {
 		private final int idx, keycode;
+		private final String cmdName;
 		public char prvState = "0".charAt(0);
 
-		public KeyMapEntry(int idx, int cmd) {
+		public KeyMapEntry(int idx, int cmd, String cmdName) {
 			this.idx = idx - 1;
 			this.keycode = cmd;
+			this.cmdName = cmdName;
 		}
 
 		public String reportIfChanged(char[] newState) {
 			char cur = newState[this.idx];
-			if (cur > prvState) return " Press:" + this.keycode;
-			else if (cur < prvState) return " Release:" + this.keycode;
+			if (cur > prvState) return " Press(" + this.keycode + "):" + this.cmdName;
+			else if (cur < prvState) return " Release(" + this.keycode + "):" + this.cmdName;
 			return "";
 		}
 
@@ -147,7 +149,24 @@ public class PixelsToKeys extends JFrame {
 				if (cur > prvState) mouseSpeedRight = 1;
 				else if (cur < prvState) mouseSpeedRight = 0;
 				break;
-			}
+
+			case 10: // VM_MOVE_10_UP
+				if (cur > prvState) mouseSpeedUp = 10;
+				else if (cur < prvState) mouseSpeedUp = 0;
+				break;
+			case 11: // VM_MOVE_10_DOWN
+				if (cur > prvState) mouseSpeedDown = 10;
+				else if (cur < prvState) mouseSpeedDown = 0;
+				break;
+			case 12: // VM_MOVE_10_LEFT
+				if (cur > prvState) mouseSpeedLeft = 10;
+				else if (cur < prvState) mouseSpeedLeft = 0;
+				break;
+			case 13: // VM_MOVE_10_RIGHT
+				if (cur > prvState) mouseSpeedRight = 10;
+				else if (cur < prvState) mouseSpeedRight = 0;
+				break;
+}
 		}
 	}
 
@@ -217,129 +236,133 @@ public class PixelsToKeys extends JFrame {
 		tKeyMapEntries.clear();
 		tMouseMapEntries.clear();
 		{
-			tKeyMapEntries.add(new KeyMapEntry(1, 8)); // VK_BACK_SPACE
-			tKeyMapEntries.add(new KeyMapEntry(2, 9)); // VK_TAB
-			tKeyMapEntries.add(new KeyMapEntry(3, 10)); // VK_ENTER
-			tKeyMapEntries.add(new KeyMapEntry(4, 12)); // VK_CLEAR
-			tKeyMapEntries.add(new KeyMapEntry(5, 16)); // VK_SHIFT
-			tKeyMapEntries.add(new KeyMapEntry(6, 17)); // VK_CONTROL
-			tKeyMapEntries.add(new KeyMapEntry(7, 18)); // VK_ALT
-			tKeyMapEntries.add(new KeyMapEntry(8, 19)); // VK_PAUSE
-			tKeyMapEntries.add(new KeyMapEntry(9, 20)); // VK_CAPS_LOCK
-			tKeyMapEntries.add(new KeyMapEntry(10, 27)); // VK_ESCAPE
-			tKeyMapEntries.add(new KeyMapEntry(11, 32)); // VK_SPACE
-			tKeyMapEntries.add(new KeyMapEntry(12, 33)); // VK_PAGE_UP
-			tKeyMapEntries.add(new KeyMapEntry(13, 34)); // VK_PAGE_DOWN
-			tKeyMapEntries.add(new KeyMapEntry(14, 35)); // VK_END
-			tKeyMapEntries.add(new KeyMapEntry(15, 36)); // VK_HOME
-			tKeyMapEntries.add(new KeyMapEntry(16, 37)); // VK_LEFT
-			tKeyMapEntries.add(new KeyMapEntry(17, 38)); // VK_UP
-			tKeyMapEntries.add(new KeyMapEntry(18, 39)); // VK_RIGHT
-			tKeyMapEntries.add(new KeyMapEntry(19, 40)); // VK_DOWN
-			tKeyMapEntries.add(new KeyMapEntry(20, 44)); // VK_COMMA
-			tKeyMapEntries.add(new KeyMapEntry(21, 45)); // VK_MINUS
-			tKeyMapEntries.add(new KeyMapEntry(22, 46)); // VK_PERIOD
-			tKeyMapEntries.add(new KeyMapEntry(23, 47)); // VK_SLASH
-			tKeyMapEntries.add(new KeyMapEntry(24, 48)); // VK_0
-			tKeyMapEntries.add(new KeyMapEntry(25, 49)); // VK_1
-			tKeyMapEntries.add(new KeyMapEntry(26, 50)); // VK_2
-			tKeyMapEntries.add(new KeyMapEntry(27, 51)); // VK_3
-			tKeyMapEntries.add(new KeyMapEntry(28, 52)); // VK_4
-			tKeyMapEntries.add(new KeyMapEntry(29, 53)); // VK_5
-			tKeyMapEntries.add(new KeyMapEntry(30, 54)); // VK_6
-			tKeyMapEntries.add(new KeyMapEntry(31, 55)); // VK_7
-			tKeyMapEntries.add(new KeyMapEntry(32, 56)); // VK_8
-			tKeyMapEntries.add(new KeyMapEntry(33, 57)); // VK_9
-			tKeyMapEntries.add(new KeyMapEntry(34, 59)); // VK_SEMICOLON
-			tKeyMapEntries.add(new KeyMapEntry(35, 61)); // VK_EQUALS
-			tKeyMapEntries.add(new KeyMapEntry(36, 65)); // VK_A
-			tKeyMapEntries.add(new KeyMapEntry(37, 66)); // VK_B
-			tKeyMapEntries.add(new KeyMapEntry(38, 67)); // VK_C
-			tKeyMapEntries.add(new KeyMapEntry(39, 68)); // VK_D
-			tKeyMapEntries.add(new KeyMapEntry(40, 69)); // VK_E
-			tKeyMapEntries.add(new KeyMapEntry(41, 70)); // VK_F
-			tKeyMapEntries.add(new KeyMapEntry(42, 71)); // VK_G
-			tKeyMapEntries.add(new KeyMapEntry(43, 72)); // VK_H
-			tKeyMapEntries.add(new KeyMapEntry(44, 73)); // VK_I
-			tKeyMapEntries.add(new KeyMapEntry(45, 74)); // VK_J
-			tKeyMapEntries.add(new KeyMapEntry(46, 75)); // VK_K
-			tKeyMapEntries.add(new KeyMapEntry(47, 76)); // VK_L
-			tKeyMapEntries.add(new KeyMapEntry(48, 77)); // VK_M
-			tKeyMapEntries.add(new KeyMapEntry(49, 78)); // VK_N
-			tKeyMapEntries.add(new KeyMapEntry(50, 79)); // VK_O
-			tKeyMapEntries.add(new KeyMapEntry(51, 80)); // VK_P
-			tKeyMapEntries.add(new KeyMapEntry(52, 81)); // VK_Q
-			tKeyMapEntries.add(new KeyMapEntry(53, 82)); // VK_R
-			tKeyMapEntries.add(new KeyMapEntry(54, 83)); // VK_S
-			tKeyMapEntries.add(new KeyMapEntry(55, 84)); // VK_T
-			tKeyMapEntries.add(new KeyMapEntry(56, 85)); // VK_U
-			tKeyMapEntries.add(new KeyMapEntry(57, 86)); // VK_V
-			tKeyMapEntries.add(new KeyMapEntry(58, 87)); // VK_W
-			tKeyMapEntries.add(new KeyMapEntry(59, 88)); // VK_X
-			tKeyMapEntries.add(new KeyMapEntry(60, 89)); // VK_Y
-			tKeyMapEntries.add(new KeyMapEntry(61, 90)); // VK_Z
-			tKeyMapEntries.add(new KeyMapEntry(62, 91)); // VK_OPEN_BRACKET
-			tKeyMapEntries.add(new KeyMapEntry(63, 92)); // VK_BACK_SLASH
-			tKeyMapEntries.add(new KeyMapEntry(64, 93)); // VK_CLOSE_BRACKET
-			tKeyMapEntries.add(new KeyMapEntry(65, 96)); // VK_NUMPAD0
-			tKeyMapEntries.add(new KeyMapEntry(66, 97)); // VK_NUMPAD1
-			tKeyMapEntries.add(new KeyMapEntry(67, 98)); // VK_NUMPAD2
-			tKeyMapEntries.add(new KeyMapEntry(68, 99)); // VK_NUMPAD3
-			tKeyMapEntries.add(new KeyMapEntry(69, 100)); // VK_NUMPAD4
-			tKeyMapEntries.add(new KeyMapEntry(70, 101)); // VK_NUMPAD5
-			tKeyMapEntries.add(new KeyMapEntry(71, 102)); // VK_NUMPAD6
-			tKeyMapEntries.add(new KeyMapEntry(72, 103)); // VK_NUMPAD7
-			tKeyMapEntries.add(new KeyMapEntry(73, 104)); // VK_NUMPAD8
-			tKeyMapEntries.add(new KeyMapEntry(74, 105)); // VK_NUMPAD9
-			tKeyMapEntries.add(new KeyMapEntry(75, 106)); // VK_MULTIPLY
-			tKeyMapEntries.add(new KeyMapEntry(76, 107)); // VK_ADD
-			tKeyMapEntries.add(new KeyMapEntry(77, 108)); // VK_SEPARATER
-			tKeyMapEntries.add(new KeyMapEntry(78, 108)); // VK_SEPARATOR
-			tKeyMapEntries.add(new KeyMapEntry(79, 109)); // VK_SUBTRACT
-			tKeyMapEntries.add(new KeyMapEntry(80, 110)); // VK_DECIMAL
-			tKeyMapEntries.add(new KeyMapEntry(81, 111)); // VK_DIVIDE
-			tKeyMapEntries.add(new KeyMapEntry(82, 112)); // VK_F1
-			tKeyMapEntries.add(new KeyMapEntry(83, 113)); // VK_F2
-			tKeyMapEntries.add(new KeyMapEntry(84, 114)); // VK_F3
-			tKeyMapEntries.add(new KeyMapEntry(85, 115)); // VK_F4
-			tKeyMapEntries.add(new KeyMapEntry(86, 116)); // VK_F5
-			tKeyMapEntries.add(new KeyMapEntry(87, 117)); // VK_F6
-			tKeyMapEntries.add(new KeyMapEntry(88, 118)); // VK_F7
-			tKeyMapEntries.add(new KeyMapEntry(89, 119)); // VK_F8
-			tKeyMapEntries.add(new KeyMapEntry(90, 120)); // VK_F9
-			tKeyMapEntries.add(new KeyMapEntry(91, 121)); // VK_F10
-			tKeyMapEntries.add(new KeyMapEntry(92, 122)); // VK_F11
-			tKeyMapEntries.add(new KeyMapEntry(93, 123)); // VK_F12
-			tKeyMapEntries.add(new KeyMapEntry(94, 127)); // VK_DELETE
-			tKeyMapEntries.add(new KeyMapEntry(95, 144)); // VK_NUM_LOCK
-			tKeyMapEntries.add(new KeyMapEntry(96, 145)); // VK_SCROLL_LOCK
-			tKeyMapEntries.add(new KeyMapEntry(97, 150)); // VK_AMPERSAND
-			tKeyMapEntries.add(new KeyMapEntry(98, 151)); // VK_ASTERISK
-			tKeyMapEntries.add(new KeyMapEntry(99, 152)); // VK_QUOTEDBL
-			tKeyMapEntries.add(new KeyMapEntry(100, 153)); // VK_LESS
-			tKeyMapEntries.add(new KeyMapEntry(101, 154)); // VK_PRINTSCREEN
-			tKeyMapEntries.add(new KeyMapEntry(102, 155)); // VK_INSERT
-			tKeyMapEntries.add(new KeyMapEntry(103, 160)); // VK_GREATER
-			tKeyMapEntries.add(new KeyMapEntry(104, 161)); // VK_BRACELEFT
-			tKeyMapEntries.add(new KeyMapEntry(105, 162)); // VK_BRACERIGHT
-			tKeyMapEntries.add(new KeyMapEntry(106, 192)); // VK_BACK_QUOTE
-			tKeyMapEntries.add(new KeyMapEntry(107, 222)); // VK_QUOTE
-			tKeyMapEntries.add(new KeyMapEntry(108, 224)); // VK_KP_UP
-			tKeyMapEntries.add(new KeyMapEntry(109, 225)); // VK_KP_DOWN
-			tKeyMapEntries.add(new KeyMapEntry(110, 226)); // VK_KP_LEFT
-			tKeyMapEntries.add(new KeyMapEntry(111, 227)); // VK_KP_RIGHT
-			tKeyMapEntries.add(new KeyMapEntry(112, 524)); // VK_WINDOWS
-			tMouseMapEntries.add(new MouseMapEntry(113, 1, "VM_BTN_LEFT")); // VM_BTN_LEFT
-			tMouseMapEntries.add(new MouseMapEntry(114, 2, "VM_BTN_MIDDLE")); // VM_BTN_MIDDLE
-			tMouseMapEntries.add(new MouseMapEntry(115, 3, "VM_BTN_RIGHT")); // VM_BTN_RIGHT
-			tMouseMapEntries.add(new MouseMapEntry(116, 4, "VM_WHEEL_UP")); // VM_WHEEL_UP
-			tMouseMapEntries.add(new MouseMapEntry(117, 5, "VM_WHEEL_DOWN")); // VM_WHEEL_DOWN
-			tMouseMapEntries.add(new MouseMapEntry(118, 6, "VM_MOVE_UP")); // VM_MOVE_UP
-			tMouseMapEntries.add(new MouseMapEntry(119, 7, "VM_MOVE_DOWN")); // VM_MOVE_DOWN
-			tMouseMapEntries.add(new MouseMapEntry(120, 8, "VM_MOVE_LEFT")); // VM_MOVE_LEFT
-			tMouseMapEntries.add(new MouseMapEntry(121, 9, "VM_MOVE_RIGHT")); // VM_MOVE_RIGHT
+			tKeyMapEntries.add(new KeyMapEntry(1, 8, "VK_BACK_SPACE"));
+			tKeyMapEntries.add(new KeyMapEntry(2, 9, "VK_TAB"));
+			tKeyMapEntries.add(new KeyMapEntry(3, 10, "VK_ENTER"));
+			tKeyMapEntries.add(new KeyMapEntry(4, 12, "VK_CLEAR"));
+			tKeyMapEntries.add(new KeyMapEntry(5, 16, "VK_SHIFT"));
+			tKeyMapEntries.add(new KeyMapEntry(6, 17, "VK_CONTROL"));
+			tKeyMapEntries.add(new KeyMapEntry(7, 18, "VK_ALT"));
+			tKeyMapEntries.add(new KeyMapEntry(8, 19, "VK_PAUSE"));
+			tKeyMapEntries.add(new KeyMapEntry(9, 20, "VK_CAPS_LOCK"));
+			tKeyMapEntries.add(new KeyMapEntry(10, 27, "VK_ESCAPE"));
+			tKeyMapEntries.add(new KeyMapEntry(11, 32, "VK_SPACE"));
+			tKeyMapEntries.add(new KeyMapEntry(12, 33, "VK_PAGE_UP"));
+			tKeyMapEntries.add(new KeyMapEntry(13, 34, "VK_PAGE_DOWN"));
+			tKeyMapEntries.add(new KeyMapEntry(14, 35, "VK_END"));
+			tKeyMapEntries.add(new KeyMapEntry(15, 36, "VK_HOME"));
+			tKeyMapEntries.add(new KeyMapEntry(16, 37, "VK_LEFT"));
+			tKeyMapEntries.add(new KeyMapEntry(17, 38, "VK_UP"));
+			tKeyMapEntries.add(new KeyMapEntry(18, 39, "VK_RIGHT"));
+			tKeyMapEntries.add(new KeyMapEntry(19, 40, "VK_DOWN"));
+			tKeyMapEntries.add(new KeyMapEntry(20, 44, "VK_COMMA"));
+			tKeyMapEntries.add(new KeyMapEntry(21, 45, "VK_MINUS"));
+			tKeyMapEntries.add(new KeyMapEntry(22, 46, "VK_PERIOD"));
+			tKeyMapEntries.add(new KeyMapEntry(23, 47, "VK_SLASH"));
+			tKeyMapEntries.add(new KeyMapEntry(24, 48, "VK_0"));
+			tKeyMapEntries.add(new KeyMapEntry(25, 49, "VK_1"));
+			tKeyMapEntries.add(new KeyMapEntry(26, 50, "VK_2"));
+			tKeyMapEntries.add(new KeyMapEntry(27, 51, "VK_3"));
+			tKeyMapEntries.add(new KeyMapEntry(28, 52, "VK_4"));
+			tKeyMapEntries.add(new KeyMapEntry(29, 53, "VK_5"));
+			tKeyMapEntries.add(new KeyMapEntry(30, 54, "VK_6"));
+			tKeyMapEntries.add(new KeyMapEntry(31, 55, "VK_7"));
+			tKeyMapEntries.add(new KeyMapEntry(32, 56, "VK_8"));
+			tKeyMapEntries.add(new KeyMapEntry(33, 57, "VK_9"));
+			tKeyMapEntries.add(new KeyMapEntry(34, 59, "VK_SEMICOLON"));
+			tKeyMapEntries.add(new KeyMapEntry(35, 61, "VK_EQUALS"));
+			tKeyMapEntries.add(new KeyMapEntry(36, 65, "VK_A"));
+			tKeyMapEntries.add(new KeyMapEntry(37, 66, "VK_B"));
+			tKeyMapEntries.add(new KeyMapEntry(38, 67, "VK_C"));
+			tKeyMapEntries.add(new KeyMapEntry(39, 68, "VK_D"));
+			tKeyMapEntries.add(new KeyMapEntry(40, 69, "VK_E"));
+			tKeyMapEntries.add(new KeyMapEntry(41, 70, "VK_F"));
+			tKeyMapEntries.add(new KeyMapEntry(42, 71, "VK_G"));
+			tKeyMapEntries.add(new KeyMapEntry(43, 72, "VK_H"));
+			tKeyMapEntries.add(new KeyMapEntry(44, 73, "VK_I"));
+			tKeyMapEntries.add(new KeyMapEntry(45, 74, "VK_J"));
+			tKeyMapEntries.add(new KeyMapEntry(46, 75, "VK_K"));
+			tKeyMapEntries.add(new KeyMapEntry(47, 76, "VK_L"));
+			tKeyMapEntries.add(new KeyMapEntry(48, 77, "VK_M"));
+			tKeyMapEntries.add(new KeyMapEntry(49, 78, "VK_N"));
+			tKeyMapEntries.add(new KeyMapEntry(50, 79, "VK_O"));
+			tKeyMapEntries.add(new KeyMapEntry(51, 80, "VK_P"));
+			tKeyMapEntries.add(new KeyMapEntry(52, 81, "VK_Q"));
+			tKeyMapEntries.add(new KeyMapEntry(53, 82, "VK_R"));
+			tKeyMapEntries.add(new KeyMapEntry(54, 83, "VK_S"));
+			tKeyMapEntries.add(new KeyMapEntry(55, 84, "VK_T"));
+			tKeyMapEntries.add(new KeyMapEntry(56, 85, "VK_U"));
+			tKeyMapEntries.add(new KeyMapEntry(57, 86, "VK_V"));
+			tKeyMapEntries.add(new KeyMapEntry(58, 87, "VK_W"));
+			tKeyMapEntries.add(new KeyMapEntry(59, 88, "VK_X"));
+			tKeyMapEntries.add(new KeyMapEntry(60, 89, "VK_Y"));
+			tKeyMapEntries.add(new KeyMapEntry(61, 90, "VK_Z"));
+			tKeyMapEntries.add(new KeyMapEntry(62, 91, "VK_OPEN_BRACKET"));
+			tKeyMapEntries.add(new KeyMapEntry(63, 92, "VK_BACK_SLASH"));
+			tKeyMapEntries.add(new KeyMapEntry(64, 93, "VK_CLOSE_BRACKET"));
+			tKeyMapEntries.add(new KeyMapEntry(65, 96, "VK_NUMPAD0"));
+			tKeyMapEntries.add(new KeyMapEntry(66, 97, "VK_NUMPAD1"));
+			tKeyMapEntries.add(new KeyMapEntry(67, 98, "VK_NUMPAD2"));
+			tKeyMapEntries.add(new KeyMapEntry(68, 99, "VK_NUMPAD3"));
+			tKeyMapEntries.add(new KeyMapEntry(69, 100, "VK_NUMPAD4"));
+			tKeyMapEntries.add(new KeyMapEntry(70, 101, "VK_NUMPAD5"));
+			tKeyMapEntries.add(new KeyMapEntry(71, 102, "VK_NUMPAD6"));
+			tKeyMapEntries.add(new KeyMapEntry(72, 103, "VK_NUMPAD7"));
+			tKeyMapEntries.add(new KeyMapEntry(73, 104, "VK_NUMPAD8"));
+			tKeyMapEntries.add(new KeyMapEntry(74, 105, "VK_NUMPAD9"));
+			tKeyMapEntries.add(new KeyMapEntry(75, 106, "VK_MULTIPLY"));
+			tKeyMapEntries.add(new KeyMapEntry(76, 107, "VK_ADD"));
+			tKeyMapEntries.add(new KeyMapEntry(77, 108, "VK_SEPARATER"));
+			tKeyMapEntries.add(new KeyMapEntry(78, 108, "VK_SEPARATOR"));
+			tKeyMapEntries.add(new KeyMapEntry(79, 109, "VK_SUBTRACT"));
+			tKeyMapEntries.add(new KeyMapEntry(80, 110, "VK_DECIMAL"));
+			tKeyMapEntries.add(new KeyMapEntry(81, 111, "VK_DIVIDE"));
+			tKeyMapEntries.add(new KeyMapEntry(82, 112, "VK_F1"));
+			tKeyMapEntries.add(new KeyMapEntry(83, 113, "VK_F2"));
+			tKeyMapEntries.add(new KeyMapEntry(84, 114, "VK_F3"));
+			tKeyMapEntries.add(new KeyMapEntry(85, 115, "VK_F4"));
+			tKeyMapEntries.add(new KeyMapEntry(86, 116, "VK_F5"));
+			tKeyMapEntries.add(new KeyMapEntry(87, 117, "VK_F6"));
+			tKeyMapEntries.add(new KeyMapEntry(88, 118, "VK_F7"));
+			tKeyMapEntries.add(new KeyMapEntry(89, 119, "VK_F8"));
+			tKeyMapEntries.add(new KeyMapEntry(90, 120, "VK_F9"));
+			tKeyMapEntries.add(new KeyMapEntry(91, 121, "VK_F10"));
+			tKeyMapEntries.add(new KeyMapEntry(92, 122, "VK_F11"));
+			tKeyMapEntries.add(new KeyMapEntry(93, 123, "VK_F12"));
+			tKeyMapEntries.add(new KeyMapEntry(94, 127, "VK_DELETE"));
+			tKeyMapEntries.add(new KeyMapEntry(95, 144, "VK_NUM_LOCK"));
+			tKeyMapEntries.add(new KeyMapEntry(96, 145, "VK_SCROLL_LOCK"));
+			tKeyMapEntries.add(new KeyMapEntry(97, 150, "VK_AMPERSAND"));
+			tKeyMapEntries.add(new KeyMapEntry(98, 151, "VK_ASTERISK"));
+			tKeyMapEntries.add(new KeyMapEntry(99, 152, "VK_QUOTEDBL"));
+			tKeyMapEntries.add(new KeyMapEntry(100, 153, "VK_LESS"));
+			tKeyMapEntries.add(new KeyMapEntry(101, 154, "VK_PRINTSCREEN"));
+			tKeyMapEntries.add(new KeyMapEntry(102, 155, "VK_INSERT"));
+			tKeyMapEntries.add(new KeyMapEntry(103, 160, "VK_GREATER"));
+			tKeyMapEntries.add(new KeyMapEntry(104, 161, "VK_BRACELEFT"));
+			tKeyMapEntries.add(new KeyMapEntry(105, 162, "VK_BRACERIGHT"));
+			tKeyMapEntries.add(new KeyMapEntry(106, 192, "VK_BACK_QUOTE"));
+			tKeyMapEntries.add(new KeyMapEntry(107, 222, "VK_QUOTE"));
+			tKeyMapEntries.add(new KeyMapEntry(108, 224, "VK_KP_UP"));
+			tKeyMapEntries.add(new KeyMapEntry(109, 225, "VK_KP_DOWN"));
+			tKeyMapEntries.add(new KeyMapEntry(110, 226, "VK_KP_LEFT"));
+			tKeyMapEntries.add(new KeyMapEntry(111, 227, "VK_KP_RIGHT"));
+			tKeyMapEntries.add(new KeyMapEntry(112, 524, "VK_WINDOWS"));
+			tMouseMapEntries.add(new MouseMapEntry(113, 1, "VM_BTN_LEFT"));
+			tMouseMapEntries.add(new MouseMapEntry(114, 2, "VM_BTN_MIDDLE"));
+			tMouseMapEntries.add(new MouseMapEntry(115, 3, "VM_BTN_RIGHT"));
+			tMouseMapEntries.add(new MouseMapEntry(116, 4, "VM_WHEEL_UP"));
+			tMouseMapEntries.add(new MouseMapEntry(117, 5, "VM_WHEEL_DOWN"));
+			tMouseMapEntries.add(new MouseMapEntry(118, 6, "VM_MOVE_UP"));
+			tMouseMapEntries.add(new MouseMapEntry(119, 7, "VM_MOVE_DOWN"));
+			tMouseMapEntries.add(new MouseMapEntry(120, 8, "VM_MOVE_LEFT"));
+			tMouseMapEntries.add(new MouseMapEntry(121, 9, "VM_MOVE_RIGHT"));
+			tMouseMapEntries.add(new MouseMapEntry(122, 10, "VM_MOVE_10_UP"));
+			tMouseMapEntries.add(new MouseMapEntry(123, 11, "VM_MOVE_10_DOWN"));
+			tMouseMapEntries.add(new MouseMapEntry(124, 12, "VM_MOVE_10_LEFT"));
+			tMouseMapEntries.add(new MouseMapEntry(125, 13, "VM_MOVE_10_RIGHT"));
 
-		}
+}
 	}
 
 	static Runnable taskPressKeys = () -> {
